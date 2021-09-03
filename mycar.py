0129678bb8d44d1b6294b965cb7e1c980a24bd30
zwzhu <zwzhu@corp.netease.com>
2021-09-03 13:05:21 +0800

    测试代码

--- mycar.py
+++ mycar.py
@@ -1,3 +1,20 @@
 # encoding=utf8
 
 
+import RPi.GPIO  as GPIO 
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+GPIO.output(16,1),GPIO.output(20,1),GPIO.output(21,0)
+
+
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.IN)
+
+print(GPIO.input(16),GPIO.input(20),GPIO.input(21))
