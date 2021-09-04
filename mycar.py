7a3484cb2f6d314289adbdd42be7c9cd42732bbe
zwzhu <zwzhu@corp.netease.com>
2021-09-04 18:03:34 +0800

    修改键盘监控方式、增加终止脚本的快捷键 、增加左右转弯处理逻辑

--- mycar.py
+++ mycar.py
@@ -2,44 +2,43 @@
 
 import time ,datetime 
 import sys 
-import keyboard
 import threading  
-from queue import Queue 
+import get_char
 
-#import RPi.GPIO  as GPIO 
+import RPi.GPIO  as GPIO 
 
-class GPIO:
-    BCM = 0 
-    OUT = 0 
-    IN = 0 
-    LOW = 0 
-    HIGH = 1 
-    def setmode(self,*arg,**kws):
-        pass 
-    def output(self,*arg,**kws):
-        pass 
-    def PWM(self,*arg,**kws):
-        class _PWM:
-            def ChangeDutyCycle(self,*arg,**kws):
-                pass 
-            def start(self,*arg,**kws):
-                pass 
+# class GPIO:
+#     BCM = 0 
+#     OUT = 0 
+#     IN = 0 
+#     LOW = 0 
+#     HIGH = 1 
+#     def setmode(self,*arg,**kws):
+#         pass 
+#     def output(self,*arg,**kws):
+#         pass 
+#     def PWM(self,*arg,**kws):
+#         class _PWM:
+#             def ChangeDutyCycle(self,*arg,**kws):
+#                 pass 
+#             def start(self,*arg,**kws):
+#                 pass 
              
-        return _PWM() 
+#         return _PWM() 
 
 
 GPIO.setmode(GPIO.BCM)
 
-A_EN_PIN = 16 
+A_EN_PIN = 21 
 A_INPUT1 = 20 
-A_INPUT2 = 21 
+A_INPUT2 = 16 
 
-B_EN_PIN = 0 
-B_INPUT1 = 0 
-B_INPUT2 = 0
+B_EN_PIN = 13 
+B_INPUT1 = 19
+B_INPUT2 = 26
 
 
-eq  = Queue() 
+
 elock=threading.RLock() 
 
 
@@ -47,12 +46,15 @@ UP_LEVEL = 0
 DOWN_LEVEL=0
 LEFT_LEVEL=0
 RIGHT_LEVEL=0 
-STEP = 2 
+STEP = 5
 MAX_LEVEL = 100
-
+G_STOP = False # 结束标示  
 
 
 class Motor:
+    STOP= 0
+    FWD = 1
+    REV = -1 
     def __init__(self,en_pin,pin1,pin2):
         self.en_pin = en_pin
         self.pin1 = pin1
@@ -63,39 +65,41 @@ class Motor:
         # 100HZ 
         self.pwm = GPIO.PWM(en_pin, 100)
         self.pwm.start(0)
+        self.state = self.STOP
     
     def fwd(self,speed):
         """正转"""
+        print("fwd %d,%d,%d,%d" % (self.en_pin,self.pin1,self.pin2,speed),"\r")
         GPIO.output(self.pin1,GPIO.HIGH)
         GPIO.output(self.pin2,GPIO.LOW)
         self.pwm.ChangeDutyCycle(speed)
+        self.state = self.FWD
     def rev(self,speed):
         """反转"""
+        print("rev %d,%d,%d,%d" % (self.en_pin,self.pin1,self.pin2,speed),"\r")
         GPIO.output(self.pin1,GPIO.LOW)
         GPIO.output(self.pin2,GPIO.HIGH)
         self.pwm.ChangeDutyCycle(speed)
+        self.state = self.REV
 
     def stop(self):
+        if self.STOP == self.state:
+            return 
+        print("stop %d,%d,%d" % (self.en_pin,self.pin1,self.pin2),"\r")
         GPIO.output(self.pin1,GPIO.LOW)
         GPIO.output(self.pin2,GPIO.LOW)
+        self.state = self.STOP
 
     
     
 
-
+ma = Motor(A_EN_PIN,A_INPUT1,A_INPUT2)
+mb = Motor(B_EN_PIN,B_INPUT1,B_INPUT2)
 
     
 
 
-def hook_keyboard(e):
-    if e.name == "up" and e.event_type == "down" :
-        eq.put(e) 
-    if e.name == "down" and e.event_type == "down" :
-        eq.put(e) 
-    if e.name == "left" and e.event_type == "down" :
-        eq.put(e) 
-    if e.name == "right" and e.event_type == "down" :
-        eq.put(e) 
+
 
 
 
@@ -105,16 +109,35 @@ def show_info():
     global DOWN_LEVEL
     global LEFT_LEVEL
     global RIGHT_LEVEL 
-    while 1:
+    global G_STOP 
+    while not G_STOP:
         elock.acquire()
         print(
             "UP_LEVEL    %d " % UP_LEVEL,
             "DOWN_LEVEL  %d " % DOWN_LEVEL,
             "LEFT_LEVEL  %d " % LEFT_LEVEL,
-            "RIGHT_LEVEL %d " % RIGHT_LEVEL,
+            "RIGHT_LEVEL %d\r" % RIGHT_LEVEL,
         )
         elock.release() 
         time.sleep(0.5)
+    print("show_info stop ")
+
+def update_motor():
+    if UP_LEVEL>0:
+        ma.fwd(UP_LEVEL)
+        mb.fwd(UP_LEVEL)
+    if DOWN_LEVEL>0:
+        ma.rev(DOWN_LEVEL)
+        mb.rev(DOWN_LEVEL) 
+    if LEFT_LEVEL>0 :
+        mb.fwd(LEFT_LEVEL) 
+    if RIGHT_LEVEL>0 :
+        ma.fwd(RIGHT_LEVEL) 
+            
+    if UP_LEVEL == 0 and DOWN_LEVEL == 0 and RIGHT_LEVEL == 0 and LEFT_LEVEL == 0:
+        ma.stop() 
+        mb.stop() 
+    
 
 def level_change(e):
     global UP_LEVEL
@@ -122,32 +145,37 @@ def level_change(e):
     global LEFT_LEVEL
     global RIGHT_LEVEL 
     elock.acquire()
-    if e.name == "up" and UP_LEVEL < MAX_LEVEL:
+    if e["name"] == "up" and UP_LEVEL < MAX_LEVEL:
         if UP_LEVEL > MAX_LEVEL/3:
             UP_LEVEL += 2*STEP
         else:
             UP_LEVEL += STEP
         UP_LEVEL = min(UP_LEVEL, MAX_LEVEL)
         DOWN_LEVEL=0
-    if e.name == "down" and DOWN_LEVEL < MAX_LEVEL:
+    if e["name"] == "down" and DOWN_LEVEL < MAX_LEVEL:
         DOWN_LEVEL += STEP
         UP_LEVEL=0
-    if e.name == "left" and LEFT_LEVEL < MAX_LEVEL:
+    if e["name"] == "left" and LEFT_LEVEL < MAX_LEVEL:
         LEFT_LEVEL += STEP
         RIGHT_LEVEL= 0 
-    if e.name == "right" and RIGHT_LEVEL < MAX_LEVEL:
+    if e["name"] == "right" and RIGHT_LEVEL < MAX_LEVEL:
         RIGHT_LEVEL += STEP
         LEFT_LEVEL=0 
+    update_motor()
     elock.release() 
-        
+    
+
+
+
 
 def auto_stop():
-    print("auto_stop start ")
+    print("auto_stop start \r")
     global UP_LEVEL
     global DOWN_LEVEL
     global LEFT_LEVEL
     global RIGHT_LEVEL 
-    while 1:
+    global G_STOP 
+    while not G_STOP:
         elock.acquire()
         if UP_LEVEL>0:
             UP_LEVEL   -= STEP
@@ -157,20 +185,55 @@ def auto_stop():
             LEFT_LEVEL -= STEP
         if RIGHT_LEVEL>0:
             RIGHT_LEVEL-= STEP
+        update_motor()
         elock.release() 
-        time.sleep(0.06)
+        time.sleep(0.2)
+    print("auto_stop stop \r")
 
 
-threading.Thread(target=auto_stop,daemon=True).start()
-threading.Thread(target=show_info,daemon=True).start()
+t_auto_stop =  threading.Thread(target=auto_stop,daemon=True)
+t_auto_stop.start()
+#threading.Thread(target=show_info,daemon=True).start()
 
 
 #keyboard.hook(lambda e: print(e.to_json(), datetime.datetime.now()))
-keyboard.hook(hook_keyboard)
+#keyboard.hook(hook_keyboard)
+def catch_key():
+    c = ""
+    cmap = {
+        '\x03': {"name":"ctrlc"},
+        "\x1b[A":{"name":"up"},
+        "\x1b[B":{"name":"down"},
+        "\x1b[D":{"name":"left"},
+        "\x1b[C":{"name":"right"},
+    }
+    while 1:
+        #print([c])
+        c = get_char.getch(1) 
+        if  c == '\x03':
+            return cmap[c]
+        
+        if c == '\x1b':
+            c += get_char.getch(1) 
+            if c[1] == '[':
+                c += get_char.getch(1)
+                if c[2] in ('A','B','C','D'):
+                    return cmap[c]
 
 
-while 1:
-    e = eq.get()
-    level_change(e)
 
-    #print(sys.stdin.read(1))
+
+num = 300000
+while num:
+    num -= 1 
+    #print(num,"\r")
+    key = catch_key()
+    if key["name"] == "ctrlc":
+        break 
+    level_change(key) 
+
+G_STOP = True 
+t_auto_stop.join() 
+ma.stop() 
+mb.stop() 
+GPIO.cleanup() 
